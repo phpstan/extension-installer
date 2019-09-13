b9f4c4e9fe93373a102c982bda5b8e57f901ce33
Ondrej Mirtes <ondrej@mirtes.cz>
2019-09-13 21:06:08 +0200

    Track GeneratedConfig in Git

---
+++ src/GeneratedConfig.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\ExtensionInstaller;
+
+/**
+ * This is a stub class: it is in place only for scenarios where Composer
+ * is run with a `--no-scripts` flag, in which scenarios this stub class
+ * is not being replaced.
+ *
+ * If you are reading this docBlock inside your `vendor/` dir, then this means
+ * that phpstan/extension-installer didn't correctly install.
+ *
+ * @internal
+ */
+final class GeneratedConfig
+{
+
+	public const EXTENSIONS = [];
+
+	public const NOT_INSTALLED = [];
+
+	private function __construct()
+	{
+	}
+
+}
